daf51df6e5ecad38b554a8b5f277f60a2c1eede9
Heyu Fu <Herry Fu>
2024-01-20 12:46:05 +1100

    Rating Function Finished

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating starLength="20" />
+    <StarRating starLength="10" />
+    <StarRating />
   </React.StrictMode>
 );
