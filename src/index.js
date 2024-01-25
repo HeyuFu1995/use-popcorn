9aebd228ad27ac789d6201fb2a85cffabe72a2ad
Heyu Fu <Herry Fu>
2024-01-25 17:56:55 +1100

    Fix Bugs and Add real data in

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App-v1';
 import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating starLength="20" />
+    <App />
+    {/* <StarRating starLength="20" />
     <StarRating starLength="10" />
-    <StarRating messages={["terrible", "bad", "just so so", "good", "wonderful"]} />
+    <StarRating messages={["terrible", "bad", "just so so", "good", "wonderful"]} /> */}
   </React.StrictMode>
 );
